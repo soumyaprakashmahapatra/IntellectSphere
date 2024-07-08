f29d99760d0db1ee17b4277081ff49eab03a6e39
soumyaprakashmahapatra <114506094+soumyaprakashmahapatra@users.noreply.github.com>
2024-07-08 18:13:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://astounding-nougat-8d7910.netlify.app",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
