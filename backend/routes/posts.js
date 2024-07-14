90d5e1f5fdcfc4a09e52216c9c67dac66057a2d9
soumyaprakashmahapatra <114506094+soumyaprakashmahapatra@users.noreply.github.com>
2024-07-14 20:49:59 +0530

    Update posts.js

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,7 +7,7 @@ const Comment=require('../models/Comment')
 const verifyToken = require('../verifyToken')
 
 //CREATE
-router.post("/create",verifyToken,async (req,res)=>{
+router.post("/create",async (req,res)=>{
     try{
         const newPost=new Post(req.body)
         // console.log(req.body)
@@ -23,7 +23,7 @@ router.post("/create",verifyToken,async (req,res)=>{
 })
 
 //UPDATE
-router.put("/:id",verifyToken,async (req,res)=>{
+router.put("/:id",async (req,res)=>{
     try{
        
         const updatedPost=await Post.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
